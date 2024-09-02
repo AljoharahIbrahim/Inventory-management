8c93a709897aa3a8e33e776ea4dbea3fd8ba11bc
AljoharahIbrahim <jalohydab@gmail.com>
2024-09-02 16:49:39 +0300

    add level1 and level2

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@
 
 public class Item
 {
-    private string createdDate;
+
+    public string CreatedDate { set; get; }
     public string Name { set; get; }
     public int Quantity { set; get; }
 
@@ -11,32 +12,51 @@ public class Item
     {
         Name = name;
         Quantity = quantity;
-        createdDate = DateTime.Now.ToString();
+        CreatedDate = DateTime.Now.ToString();
     }
+    public Item(string name, int quantity, string createdDate)
+    {
+        Name = name;
+        Quantity = quantity;
+        CreatedDate = createdDate;
+    }
+
 }
 
 public class Store
 {
-    public List<Item> collectionList;
 
-    public Store()
+    public List<Item> collectionList;
+    private int capacity;
+    public Store(int capacity)
     {
         collectionList = new List<Item>();
+        this.capacity = capacity;
     }
 
     public void addItem(Item newItem)
     {
         try
         {
+            int capacityOfItems = 0;
             foreach (var item in collectionList)
             {
+                capacityOfItems++;
                 if (item.Name == newItem.Name)
                 {
                     throw new Exception("item name already exists in our array");
                 }
             }
-            collectionList.Add(newItem);
-            Console.WriteLine($"Added item successfully");
+            if (capacityOfItems <= capacity)
+            {
+                collectionList.Add(newItem);
+                Console.WriteLine($"Added item successfully");
+            }
+            else
+            {
+                Console.WriteLine($"you dont have a valid number of capacity to added");
+            }
+
         }
         catch (System.Exception ex)
         {
@@ -46,14 +66,23 @@ public class Store
 
     public void deleteItem(Item deletedItem)
     {
-        foreach (var item in collectionList)
+        try
         {
-            if (item.Name == deletedItem.Name)
+            foreach (var item in collectionList.ToList()) //* .ToList()
             {
-                collectionList.Remove(deletedItem);
-                Console.WriteLine($"deleted item successfully");
+                if (item.Name == deletedItem.Name)
+                {
+                    if (collectionList.Remove(deletedItem))
+                        Console.WriteLine($"deleted item successfully");
+                }
             }
         }
+        catch (System.Exception ex)
+        {
+            Console.WriteLine($"Exception {ex.Message}");
+
+        }
+
     }
 
     public void GetCurrentVolume()
@@ -73,26 +102,52 @@ public class Store
         return itemFound;
     }
 
-    public void SortByNameAsc() { }
+    public IEnumerable<Item> SortByNameAsc()
+    {
+        return collectionList.OrderBy(item => item.Name);
+    }
+
+    public IEnumerable<Item> SortByDate(){
+        return collectionList.OrderBy(item => item.CreatedDate);
+        
+    }
+      public IEnumerable<Item> SortByDateDESC(){
+        return collectionList.OrderByDescending(item => item.CreatedDate);    
+    }
+    public void GroupByDate(){ //**
+
+        string currentMonth = DateTime.Now.ToString();
+         
+    }
+
 }
 
 public class App
 {
     public static void Main(string[] args)
     {
-        Store store = new Store();
-        Item item1 = new Item("item1", 5);
-        Item item2 = new Item("item2", 6);
+        Store store = new Store(3);
+        Item item1 = new Item("Fitem1", 5);
+        Item item2 = new Item("Atem2", 6, new DateTime(2023, 7, 1).ToString());
         store.addItem(item1);
         store.addItem(item2);
-
         // // run for loop of collection to check item is added or not
         // foreach (var item in store.collectionList)
         // {
         //     Console.WriteLine($"item is print: {item.Name}");
         // }
         // store.deleteItem(item2);
-        // store.GetCurrentVolume();
-        // store.FindItemByName("item1");
+        store.GetCurrentVolume();
+        store.FindItemByName("item1");
+        var SortByNameAscList = store.SortByNameAsc();
+        foreach (var item in SortByNameAscList)
+        {
+            Console.WriteLine($"{item.Name}");
+        }
+         var SortByDateList = store.SortByDate();
+        foreach (var item in SortByNameAscList)
+        {
+            Console.WriteLine($"{item.CreatedDate}");
+        }
     }
 }
